0f6b61dd128f9ab0d2a9c6549e5a6cefcdd1c5aa
adarsh <adarshjadhav2004@gmail.com>
2024-01-30 22:08:34 +0530

    files

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="dev-8v0hjk0myl8aoi2c.us.auth0.com"
+    clientId="jzcKoy2CYrzvs3zW0Gvig6BPlNvSUFzo"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
